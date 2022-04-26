4dfd07787c4b23330162516f3de94b34f3d179ca
Dom <Domalbano35@gmail.com>
2022-04-26 13:52:07 -0400

    updated route and model folder contents to reflect user, review, and movie

---
+++ controllers/api/movieRoutes.js
@@ -0,0 +1,18 @@
+const router = require("express").Router();
+const { Movie } = require("../../models");
+const withAuth = require("../../utils/auth");
+
+router.post("/", withAuth, async (req, res) => {
+  try {
+    const newMovie = await Movie.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newMovie);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+module.exports = router;
