4e923bdbaaf7c94c1a460da89881b8883cc22cc2
Dom <Domalbano35@gmail.com>
2022-04-28 23:43:43 -0400

    heroku v12

--- controllers/api/movieRoutes.js
+++ controllers/api/movieRoutes.js
@@ -2,18 +2,14 @@ const router = require("express").Router();
 const { Movie } = require("../../models");
 const withAuth = require("../../utils/auth");
 
-// router.post("/", withAuth, async (req, res) => {
-//   try {
-//     const newMovie = await Movie.create({
-//       ...req.body,
-//       user_id: req.session.user_id,
-//     });
-
-//     res.status(200).json(newMovie);
-//   } catch (err) {
-//     res.status(400).json(err);
-//   }
-// });
+router.get("/", async (req, res) => {
+  try {
+    const movieData = await Movie.findAll({});
+    res.json(movieData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 
 // /api/movies gets data back
